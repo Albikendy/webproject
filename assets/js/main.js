c571411cf56db4edc40dcf2f66ad70de0750318c
JEAN ALBIKENDY <jeanalbikendy@gmail.com>
2024-06-18 15:03:59 -0400

    modification de la partie header pour les taille d'ecran moins de 768px

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
   const menuBtn = document.getElementById('menuBtn');
+  const closeBtn = document.getElementById('closeBtn');
   const navLinks = document.getElementById('navLinks');
+  const body = document.body; // Sélectionne l'élément body
 
   menuBtn.addEventListener('click', function() {
-    navLinks.classList.toggle('active');
+      navLinks.style.right = "0";
+      navLinks.classList.add("active");
+      menuBtn.style.display = "none";
+      closeBtn.style.display = "block";
+      body.classList.add('no-scroll'); // Désactive le défilement de la page
+  });
+
+  closeBtn.addEventListener('click', function() {
+      navLinks.style.right = "-100%";
+      navLinks.classList.remove("active");
+      menuBtn.style.display = "block";
+      closeBtn.style.dsssisplay = "none";
+      body.classList.remove('no-scroll'); // Active le défilement de la page
   });
 });
+
 
 /*============== SWIPER JS ==================*/
 let swiperCards = new Swiper('.card__content', {
@@ -33,27 +48,4 @@ let swiperCards = new Swiper('.card__content', {
       },
     },
 });
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.getElementById("menuBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  const navLinks = document.getElementById("navLinks");
-
-  // Ensure navLinks is hidden by default
-  navLinks.style.right = "-100%";
-
-  if (menuBtn && closeBtn && navLinks) {
-    menuBtn.addEventListener("click", () => {
-      navLinks.style.right = "0";
-      navLinks.classList.add("active"); // Add the background
-    });
-
-    closeBtn.addEventListener("click", () => {
-      navLinks.style.right = "-100%";
-      navLinks.classList.remove("active"); // Remove the background
-    });
-  }
-});
-
 
